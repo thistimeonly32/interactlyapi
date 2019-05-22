59607ae6c3e389c054cb1301b94014ad844ced3d
Shivam <thistimeonly32@gmail.com>
2019-05-23 04:32:13 +0530

    Demo socket

---
+++ src/main/java/com/liveinu/interactly/controller/ConnectionCont.java
@@ -0,0 +1,35 @@
+package com.liveinu.interactly.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.liveinu.interactly.dto.ConnectionDto;
+
+@Controller
+public class ConnectionCont {
+
+	List<ConnectionDto> connectionsDto = new ArrayList<>();
+
+	@MessageMapping("/connect")
+	@SendTo("/interactly-response/connection-response")
+	public ConnectionDto greeting(ConnectionDto connectionDto) throws Exception {
+		connectionsDto.add(connectionDto);
+		connectionDto.setRemotePeerId("not found");
+//		Thread.sleep(10000); // simulated delay
+		connectionsDto.remove(connectionDto);
+		return connectionDto;
+	}
+
+//	@MessageMapping("/getPeer")
+//	@SendTo("/voice/response")
+//	public Peer getPeer(Peer peer) throws Exception {
+////		Thread.sleep(1000); // simulated delay
+//		System.out.println("in get peer: " + this.peers.size());
+//		return new Peer(String.valueOf(this.peers.size()));
+//	}
+
+}
---
+++ src/main/java/com/liveinu/interactly/dto/ConnectionDto.java
@@ -0,0 +1,48 @@
+package com.liveinu.interactly.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class ConnectionDto {
+
+	private String localPeerId;
+	private String remotePeerId;
+	private String connectionMsg;
+
+	public ConnectionDto() {
+		super();
+	}
+
+	public ConnectionDto(String localPeerId, String remotePeerId, String connectionMsg) {
+		super();
+		this.localPeerId = localPeerId;
+		this.remotePeerId = remotePeerId;
+		this.connectionMsg = connectionMsg;
+	}
+
+	public String getLocalPeerId() {
+		return localPeerId;
+	}
+
+	public void setLocalPeerId(String localPeerId) {
+		this.localPeerId = localPeerId;
+	}
+
+	public String getRemotePeerId() {
+		return remotePeerId;
+	}
+
+	public void setRemotePeerId(String remotePeerId) {
+		this.remotePeerId = remotePeerId;
+	}
+
+	public String getConnectionMsg() {
+		return connectionMsg;
+	}
+
+	public void setConnectionMsg(String connectionMsg) {
+		this.connectionMsg = connectionMsg;
+	}
+
+}
